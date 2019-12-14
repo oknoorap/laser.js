53392c6f5b7b2310789d0982f53009ad6f768a8f
Ribhararnus Pracutian <oknoorap@gmail.com>
2019-12-14 15:30:57 +0700

    feat(setup): initial commit

---
+++ src/components/test.tsx
@@ -0,0 +1,7 @@
+import { FunctionComponent } from "react";
+
+const Test: FunctionComponent<{}> = () => {
+  return null;
+};
+
+export default Test;
--- src/index.tsx
+++
@@ -1,5 +0,0 @@
-const Test: FunctionComponent = () => {
-
-};
-
-export default Test;
