fdee0eccdb5f2d809eeb31a4a7172c06675e6536
Ribhararnus Pracutian <oknoorap@gmail.com>
2020-02-01 13:18:32 +0700

    fix: removed unused files

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-import Game from "./game";
-import Scene from "./scene";
-
-export { Game, Scene };
--- src/utils/fixtures.ts
+++
@@ -1,4 +0,0 @@
-export const ID_ALPHABET =
-  "1234567890abcdefghijklmnpqrstuvwxyz$-ABCDEFGHIJKLMNPQRSTUVWXYZ_";
-
-export const ID_LENGTH = 7;
