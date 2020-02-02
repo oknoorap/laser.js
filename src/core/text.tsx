2359f21e6af4e48d031e36db1a28b052881a559c
Ribhararnus Pracutian <oknoorap@gmail.com>
2020-02-02 12:44:32 +0700

    feat: add text object component

---
+++ src/core/text.tsx
@@ -0,0 +1,27 @@
+import { FC, forwardRef, RefForwardingComponent } from "react";
+import { Types } from "phaser";
+
+import createComponent, { IComponent } from "../utils/create-component";
+import { EComponentType } from "../utils/common-typings";
+
+interface IGame extends IComponent {
+  x?: number;
+  y?: number;
+  style?: Types.GameObjects.Text.TextStyle;
+}
+
+const Text: RefForwardingComponent<FC<IGame>, IGame> = forwardRef(
+  ({ children, x = 0, y = 0, ...props }, ref) =>
+    createComponent({
+      ref,
+      children,
+      type: EComponentType.Text,
+      props: {
+        x,
+        y,
+        ...props
+      }
+    })
+);
+
+export default Text;
